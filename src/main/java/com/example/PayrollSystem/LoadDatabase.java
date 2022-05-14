e6287bd0614f6250f3869c3e1208670fdc93402c
Antonio Barrera <jeantonio.barrera@gmail.com>
2022-05-13 23:46:52 -0600

    feat: employees data access implemented

---
+++ src/main/java/com/example/PayrollSystem/LoadDatabase.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.PayrollSystem;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author jeant
+ */
+@Configuration
+public class LoadDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository employeeRepository) {
+        return args -> {
+            log.info("Preloading " + employeeRepository.save(new Employee("Antonio Barrera", "developer")));
+            log.info("Preloading " + employeeRepository.save(new Employee("Ruldin Ayala", "professor")));
+            log.info("Preloading " + employeeRepository.save(new Employee("Daniela Castillo", "teacher")));
+        };
+    }
+}
